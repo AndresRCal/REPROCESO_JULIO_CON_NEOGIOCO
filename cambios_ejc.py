fc40cb97b6b83e402f8e5de49e5dbab3fac64431
erickdejesus <edejc@liverpool.com.mx>
2017-02-01 15:44:25 -0600

    cambios erick

---
+++ cambios_ejc.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 15:07:55 2017
+
+@author: edejc
+"""
+import time
+import pandas as pd
+import os
+
+def importa_intensificadores():
+    start=time.strftime("%H:%M:%S")
+    path='user\\chunker\\'
+    nfile='intensifiers.dat'
+    intens=pd.read_csv(path+os.sep+nfile,sep='\t',encoding='latin_1',header=None,names=['palabra','valor'])
+    end=time.strftime("%H:%M:%S")
+    print(' importa_General()','Inicio:  ',start,'Fin:  ',end)
+    return(intens)
